0b4c203043cf7c38e066545e654c0534ea385d26
flurrd <mhall.flurrd@gmail.com>
2022-06-04 11:15:13 +1000

    Add select input for filter, bind types and handle change / update state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
+import React, { useState, useEffect } from 'react'
 import { Page } from "@/components/Page";
 import { Main } from "@/components/Main";
 import Navbar from "@/components/Navbar";
 import {Rv, RV_TYPES} from "@/types/rvs";
 import useSWR from 'swr'
+import map from 'lodash/map';
 
 const fetcher = (url : any) => fetch(url).then((res) => res.json())
 
 
 const RvViewer = () => {
 
-  const { data, error } = useSWR('/api/rvs', fetcher)
+  const { data, error } = useSWR('/api/rvs', fetcher);
+  const [fetchUrl, setFetchUrl] = React.useState('');
+  const [selectedTypeFilter, setSelectedTypeFilter] = React.useState('');
+
+  const onFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedType = event.target.value;
+    setSelectedTypeFilter(selectedType)
+}
+
   if (error) return <div>Sorry, no results found</div>
   if (!data) return <div>Loading... No spinner component?</div>
 
@@ -20,6 +30,13 @@ const RvViewer = () => {
 
       <Main>
         <h1>Rv Viewer</h1>
+        <select name="vanType" id="vanType" onChange={onFilterChange}  /* onChange={} */>
+
+          {map(RV_TYPES, (value, key) => (
+            <option value={key} key={key}>{value}</option>
+          ))}
+
+        </select>
         {data.map((rv : Rv, i: any ) => (
 
           <article key={i}>
